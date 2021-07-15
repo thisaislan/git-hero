3d631fd1b06232bf7c24297abc7af0fe79efa827
thisaislan <this.aislan@gmail.com>
2021-07-15 19:21:17 -0300

    :sparkles: feature: custom color on note

---
+++ Git Hero/Assets/Scripts/Constants/Colors.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Githero.Constants
+{
+    public static class Colors
+    {
+        public static string FirstLineHexColor = "#A82455";
+        public static string SecondLineHexColor = "#ED8B6C";
+        public static string ThirdLineHexColor = "#3457FF";
+        public static string FourthLineHexColor = "#604383";
+    }
+
+}
---
+++ Git Hero/Assets/Scripts/GameObject/Note.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Githero.GameObject
+{
+    public class Note : MonoBehaviour
+    {
+        public void SetColor(int numberOfNote)
+        {
+            string colorHex;
+            Color color;
+
+            switch (numberOfNote)
+            {
+                case 0:
+                    colorHex = Constants.Colors.FirstLineHexColor;
+                    break;
+                case 1:
+                    colorHex = Constants.Colors.SecondLineHexColor;
+                    break;
+                case 2:
+                    colorHex = Constants.Colors.ThirdLineHexColor;
+                    break;
+                // case 3
+                default:
+                    colorHex = Constants.Colors.FourthLineHexColor;
+                    break;
+            }
+
+            if (ColorUtility.TryParseHtmlString(colorHex, out color))
+            {
+                GetComponent<Renderer>().material.color = color;
+            }
+        }
+
+    }
+
+}
