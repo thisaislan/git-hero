ac28cd630d91a006419d583a31a02da3cd8a86ba
thisaislan <this.aislan@gmail.com>
2021-07-15 11:04:32 -0300

    :sparkles: feature: create trgger helper

---
+++ Git Hero/Assets/Scripts/Helpers/TriggerHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+namespace Githero.Helpers
+{
+    public class TriggerHelper : MonoBehaviour
+    {
+        public Action<Collider> actionOnTriggerEnter { private get; set; }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            actionOnTriggerEnter?.Invoke(other);
+        }
+
+    }
+
+}
