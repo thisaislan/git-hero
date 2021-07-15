5a063e6b8fedbcaa605b9e3542565e915013732e
thisaislan <this.aislan@gmail.com>
2021-07-15 12:19:38 -0300

    :sparkles: feature: create scroll uv script

---
+++ Git Hero/Assets/Scripts/UV/ScrollUV.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollUV : MonoBehaviour
+{
+    [SerializeField]
+    private float scrollX;
+    [SerializeField]
+    private float scrollY;
+    [SerializeField]
+
+    void Update()
+    {
+        var offsetX = Time.time * scrollX;
+        var offsetY = Time.time * scrollY;
+
+        var offsetVector = new Vector2(offsetX, offsetY);
+        var renderer = GetComponent<Renderer>();
+
+        renderer.material.mainTextureOffset = offsetVector;
+    }
+}
