54dc1d1e3c1d9fc74d9bb59fe0e4b5fa850feeba
thisaislan <this.aislan@gmail.com>
2021-07-15 23:47:02 -0300

    :recycle: refactor: remove unecessary ussing

--- Git Hero/Assets/Scripts/Constants/Colors.cs
+++ Git Hero/Assets/Scripts/Constants/Colors.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-namespace Githero.Constants
+﻿namespace Githero.Constants
 {
     public static class Colors
     {
