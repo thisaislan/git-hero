0f6913d1e8e0ef197a045230bba98c7540149c8a
thisaislan <this.aislan@gmail.com>
2021-07-22 22:47:40 -0300

    :hammer: feature: add game object on collision on trigger helper

--- Git Hero/Assets/Game/Scripts/Helpers/TriggerHelper.cs
+++ Git Hero/Assets/Game/Scripts/Helpers/TriggerHelper.cs
@@ -7,17 +7,21 @@ namespace Githero.Game.Helpers
     {
         public bool OnTrigger { get; private set; }
 
+        public GameObject GameObjectOnCollision { get; private set; }
+
         public Action<Collider> ActionOnTriggerEnter { private get; set; }
 
         private void OnTriggerEnter(Collider other)
         {
             OnTrigger = true;
+            GameObjectOnCollision = other.gameObject;
             ActionOnTriggerEnter?.Invoke(other);
         }
 
         private void OnTriggerExit(Collider other)
         {
             OnTrigger = false;
+            GameObjectOnCollision = null;
         }
 
     }
